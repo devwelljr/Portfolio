4e7e9c08b37ac75b1a87d6fa9907574aba32c63b
Wellington-pc <junior.wel02@gmail.com>
2023-03-28 19:46:18 -0300

    done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="bg-dark font-small text-white footer navbar-fixed-bottom">
+    <footer className="bg-dark font-small text-white footer" style={{ bottom: 0, position: 'absolute', width: '100%' }}>
       <h3 className="text-uppercase footer-copyright fs-6">
         <span className="text-muted fs-6">© 2021 </span>
         WELLINGTON GREGORIO
